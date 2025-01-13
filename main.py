4a87053a87dd3d19d5b029e1bcac7b18ed0fa079
POWERTRICEPS <imnotdacoco05@gmail.com>
2025-01-12 23:02:17 -0800

    implemented buy and sell order

--- main.py
+++ main.py
@@ -9,4 +9,22 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 
 client = Client(API_KEY, API_SECRET, testnet = True)
-print(client.get_account())
+
+symbol = 'BTCUSDT'
+
+
+def get_current_price(symbol):
+    ticker = client.get_symbol_ticker(symbol=symbol)
+    return float(ticker['price'])
+
+
+def place_buy_order(symbol, quantity):
+    order = client.order_market_buy(symbol=symbol, quantity=quantity)
+    print(f"Buy order done: {order}")
+
+
+def place_sell_order(symbol, quantity):
+    order = client.order_market_sell(symbol=symbol, quantity=quantity)
+    print(f"Sell order done: {order}")
+    
+    
