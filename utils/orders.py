d1e88022a9f9bff6156d914c6f80c74874b6a5af
POWERTRICEPS <imnotdacoco05@gmail.com>
2025-01-15 18:49:12 -0800

    updated

--- utils/orders.py
+++ utils/orders.py
@@ -1,17 +1,25 @@
 from binance.client import Client
+from datafetch import client
 
 
-def get_current_price(symbol):
-    ticker = client.get_symbol_ticker(symbol=symbol)
-    return float(ticker['price'])
+
+#def get_current_price(symbol):
+    #ticker = client.get_symbol_ticker(symbol=symbol)
+   # return float(ticker['price'])
 
 
 def place_buy_order(symbol, quantity):
-    order = client.order_market_buy(symbol=symbol, quantity=quantity)
-    return order
+    try:
+        order = client.order_market_buy(symbol=symbol, quantity=quantity)
+        return order
+    except Exception as e:
+        return None
 
 def place_sell_order(symbol, quantity):
-    order = client.order_market_sell(symbol=symbol, quantity=quantity)
-    return order
+    try:
+        order = client.order_market_sell(symbol=symbol, quantity=quantity)
+        return order
+    except Exception as e:
+        return None
     
     
